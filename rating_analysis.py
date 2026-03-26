5a48c4191163ac56c0f20de84d6eee2a11124b01
Pares-mh <pares8u@stu.scu.edu.cn>
2026-03-26 10:44:28 +0800

    analyze the overall distribution of star rating

--- rating_analysis.py
+++ rating_analysis.py
@@ -6,3 +6,10 @@ from pyspark.sql.window import Window
 spark.sql("USE yelp_db")
 review_df = spark.table("review")
 biz_df = spark.table("business")
+
+# =============================================================================
+# IV. 1: Analyze the overall distribution of star ratings (1 to 5)
+# =============================================================================
+def run_iv1():
+    rating_dist = review_df.groupBy("rev_stars").count().orderBy("rev_stars")
+    z.show(rating_dist)
