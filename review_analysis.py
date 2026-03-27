9a6caba2f5b4daecdbdb443a25bbe7c3ca2556c2
mobashirsifat123 <2023521460117@stu.scu.edu.cn>
2026-03-27 17:06:38 +0800

    count the number of reviews per year

--- review_analysis.py
+++ review_analysis.py
@@ -7,3 +7,11 @@ spark.sql("USE yelp_db")
 review_df = spark.table("review")
 user_df = spark.table("users")
 biz_df = spark.table("business")
+
+# =============================================================================
+# III. 1. Count the number of reviews per year
+# =============================================================================
+def run_iii1():
+    reviews_per_year = review_df.withColumn("year", F.year("rev_date")) \
+        .groupBy("year").count().orderBy("year")
+    z.show(reviews_per_year)
